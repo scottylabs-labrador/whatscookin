f6a01ea488c3edd9a50d0cee8e302ed3043007c9
AutumnQiu99 <autumn.t.qiu@gmail.com>
2024-07-11 23:21:43 -0400

    added blank screens for login, feed, profile

---
+++ app/(tabs)/feed.tsx
@@ -0,0 +1,6 @@
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function FeedScreen() {
+}
