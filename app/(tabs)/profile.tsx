47b6c20821a2e734a6d97837f717955f319924e0
AutumnQiu99 <autumn.t.qiu@gmail.com>
2024-07-18 21:39:50 -0400

    display user's name

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,6 +1,11 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Image, Platform } from 'react-native';
+
 import ParallaxScrollView from '@/components/ParallaxScrollView';
+import BannerView from '@/components/BannerView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { TextBanner } from '@/components/TextBanner';
 
 // export default function ProfileScreen() {
 // }
@@ -11,19 +16,48 @@ import { Link } from "expo-router";
 export default function ProfileScreen() {
   const { user } = useUser();
 
+  console.log(user?.fullName);
+
   return (
-    <ThemedView>
-      <SignedIn>
-        <ThemedText>Hello {user?.emailAddresses[0].emailAddress}</ThemedText>
-      </SignedIn>
-      <SignedOut>
+    // <ParallaxScrollView
+    // headerText={"hello " + user?.fullName ?? ""}
+    //   headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+    //   headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+
+    <BannerView
+      bannerText={"hello " + user?.fullName ?? ""}
+    >
+      <ThemedView>
+        <SignedIn>
+          {/* <TextBanner text={"hello " + user?.fullName ?? ""} /> */}
+          <ThemedText>Hello {user?.emailAddresses[0].emailAddress}</ThemedText>
+
+        </SignedIn>
+
+        {/* TODO delete code below / below not needed?? shouldn't be able to see the profile page if not signed in?? */}
+        {/* <SignedOut>
         <Link href="/sign-in">
           <ThemedText>Sign In</ThemedText>
         </Link>
         <Link href="/sign-up">
           <ThemedText>Sign Up</ThemedText>
         </Link>
-      </SignedOut>
-    </ThemedView>
+      </SignedOut> */}
+      </ThemedView>
+    </BannerView>
+    // </ParallaxScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
