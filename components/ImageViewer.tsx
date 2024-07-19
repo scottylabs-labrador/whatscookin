2254a4c8ca98f3ded6bcb0bb707ed66b3997fbfe
AutumnQiu99 <autumn.t.qiu@gmail.com>
2024-07-19 09:45:57 -0400

    displays user selected image

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -3,14 +3,18 @@ import { StyleSheet, Image, ImageSourcePropType } from 'react-native';
 import type { PropsWithChildren } from 'react';
 
 type Props = {
-  placeholderImageSource: ImageSourcePropType ;
+  placeholderImageSource: ImageSourcePropType,
+  selectedImage: string
 };
 
 export default function ImageViewer({
   placeholderImageSource,
+  selectedImage
 }: Props) {
+  const imageSource = selectedImage != "" ? { uri: selectedImage } : placeholderImageSource;
+
   return (
-    <Image source={placeholderImageSource} style={styles.image} />
+    <Image source={imageSource} style={styles.image} />
   );
 }
 
