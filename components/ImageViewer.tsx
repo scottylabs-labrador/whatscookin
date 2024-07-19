2fa7920bf63e0912af927e352ab45683335f3666
AutumnQiu99 <autumn.t.qiu@gmail.com>
2024-07-18 22:58:45 -0400

    edited code to use Props to align with other code in app (eg TextBanner component)

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,19 +1,23 @@
 import { StyleSheet, Image, ImageSourcePropType } from 'react-native';
 
-interface ImageViewerProps {
-  placeholderImageSource: ImageSourcePropType;
-}
+import type { PropsWithChildren } from 'react';
 
-export const ImageViewer: React.FC<ImageViewerProps> = ({ placeholderImageSource }) => {
+type Props = {
+  placeholderImageSource: ImageSourcePropType ;
+};
+
+export default function ImageViewer({
+  placeholderImageSource,
+}: Props) {
   return (
     <Image source={placeholderImageSource} style={styles.image} />
   );
-};
+}
 
 const styles = StyleSheet.create({
   image: {
     width: 320,
-    height: 340,
+    height: 440,
     borderRadius: 18,
   },
 });
