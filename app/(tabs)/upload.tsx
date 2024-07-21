fcd2e8932b7a42afc4499a9836056543c8559595
AutumnQiu99 <autumn.t.qiu@gmail.com>
2024-07-20 20:46:51 -0400

    fixed page styling

--- app/(tabs)/upload.tsx
+++ app/(tabs)/upload.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -9,8 +11,11 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import ImageViewer from '@/components/ImageViewer';
 import Button from '@/components/Button';
 
-import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import TextBanner from '@/components/TextBanner';
+
+const { width, height } = Dimensions.get('window');
+// console.log(width);
+// console.log(height);
 
 const PlaceholderImage = require('../../assets/images/react-logo.png');
 
@@ -19,17 +24,17 @@ export default function UploadScreen() {
 
     const pickImageAsync = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-          allowsEditing: true,
-          quality: 1,
+            allowsEditing: true,
+            quality: 1,
         });
-    
+
         if (!result.canceled) {
             setSelectedImage(result.assets[0].uri);
-        } 
+        }
         // else {
         //   alert('You did not select any image.');
         // }
-      };
+    };
 
 
     const useImage = () => {
@@ -41,13 +46,14 @@ export default function UploadScreen() {
 
     return (
         <ThemedView style={styles.container}>
-            <SafeAreaView >
+            <SafeAreaView>
+                <TextBanner text="Upload"></TextBanner>
                 <ThemedView style={styles.imageContainer}>
-                    <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
+                    <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
                 </ThemedView>
                 <ThemedView style={styles.footerContainer}>
-                    <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
-                    <Button label="Use this photo" onPress={useImage}/>
+                    <Button label="Choose a photo" theme="primary" onPress={pickImageAsync} width={width * 0.8} height={68} />
+                    <Button label="Use this photo" onPress={useImage} width={width * 0.8} height={68} />
                 </ThemedView>
                 <StatusBar style="auto" />
             </SafeAreaView>
@@ -58,16 +64,21 @@ export default function UploadScreen() {
 
 const styles = StyleSheet.create({
     container: {
+        // display: "flex",
         flex: 1,
-        backgroundColor: '#25292e',
-        alignItems: 'center',
+        justifyContent: 'center', // Centers children vertically
+        alignItems: 'center', // Centers children horizontally
+        backgroundColor: '#fff',
     },
     imageContainer: {
-        flex: 1,
-        paddingTop: 58,
+        // flex: 1,
+        // paddingTop: 20,
+        width: width * 0.95, // 90% of the screen width
+        height: height * 0.5, // 50% of the screen height
     },
     footerContainer: {
-        flex: 1 / 3,
-        alignItems: 'center',
+        // flex: 1 / 3,
+        // alignItems: 'center',
+        top: height * 0.02
     },
 });
