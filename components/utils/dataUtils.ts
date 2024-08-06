bd66459b1975046050141f3b3a6aee84fb28ee6f
qianxuege <qianxuege15@gmail.com>
2024-08-06 17:22:43 -0400

    added fallback for getPhotoURL

--- components/utils/dataUtils.ts
+++ components/utils/dataUtils.ts
@@ -12,6 +12,7 @@ export const getPhotoURL = async (docId: string) => {
         const url = await getDownloadURL(reference);
         return url;
     } catch {
+        console.log(docId);
         return 'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg';
     }
     
